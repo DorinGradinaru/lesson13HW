656f5a9214be901e9dd24c89c920dcf2818f5cc7
DorinGradinaru <dorin.gradinaru@icloud.com>
2024-03-05 13:39:40 +0200

    first

---
+++ index.js
@@ -0,0 +1,37 @@
+//ex1
+// let conventiaCamelCaseExemplu = 'variabila in camelCase'
+// let CONVENTIA_UPPER_SNAKE_EXEMPLU = 'variabila in UPPER_SNAKE_CASE'
+// let ConventiaCapitalCaseExemplu = 'variabila in CapitalCase'
+
+
+
+
+
+// ex2
+// const nume = "Dorin"
+// const age = 19
+// const ocupation = "Student"
+
+// console.log(`Hello, my name is ${nume}. I am ${age} years old and I am a ${ocupation}.`)
+// console.log("Hello, my name is " + nume +". I am " + age + " years old and I am a " + ocupation + ".")
+
+
+
+
+
+
+// ex2
+// let num = 40
+
+// let numString = "" + num
+// console.log(typeof numString)
+
+// let numBoolean = Boolean(num)
+// console.log(typeof numBoolean)
+
+// let numBoolean = !!num;
+// console.log(typeof numBoolean)
+
+
+
+
